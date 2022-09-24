f2a5b6927dfe86239a939074fcb44730fd0727f6
Levi H <levi.heintzelman@gmail.com>
2022-09-24 13:41:05 -0600

    Create seeds

---
+++ seeds/seed.js
@@ -0,0 +1,23 @@
+const sequelize = require('../config/connection');
+const { User, Post, Comments } = require('../models');
+
+const userData = require('./user.json');
+const postData = require('./post.json');
+const commentData = require('./comments.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+  
+    await User.bulkCreate(userData, {
+      individualHooks: true,
+      returning: true,
+    });
+  
+    await Post.bulkCreate(postData);
+    await Comments.bulkCreate(commentData)
+  
+    process.exit(0);
+};
+  
+seedDatabase();
+  
